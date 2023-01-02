bdb514bd2fb5525dcc638f0cb8a8c9487e7d6210
ArthurR0drigues <108202997+ArthurR0drigues@users.noreply.github.com>
2023-01-01 22:10:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,518 @@
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+let Brasil = {
+    ger: 86,
+    continente: 'AméricaSul',
+    bandeira: 'img/brasil.png',
+    nome: 'Brasil',
+    pontos: 0
+};
+
+let Argentina = {
+    ger: 90,
+    continente: 'AméricaSul',
+    bandeira: 'img/argentina.png',
+    nome: 'Argentina',
+    pontos: 0
+};
+
+let Colombia = {
+    ger: 73,
+    continente: 'AméricaSul',
+    bandeira: 'img/colombia.png',
+    nome: 'Colombia',
+    pontos: 0
+};
+
+let Chile = {
+    ger: 75,
+    continente: 'AméricaSul',
+    bandeira: 'img/chile.png',
+    nome: 'Chile',
+    pontos: 0
+};
+
+let Venezuela = {
+    ger: 71,
+    continente: 'AméricaSul',
+    bandeira: 'img/venezuela.png',
+    nome: 'Venezuela',
+    pontos: 0
+};
+
+let Bolivia = {
+    ger: 72,
+    continente: 'AméricaSul',
+    bandeira: 'img/bolivia.png',
+    nome: 'Bolivia',
+    pontos: 0
+};
+
+let Peru = {
+    ger: 73,
+    continente: 'AméricaSul',
+    bandeira: 'img/peru.png',
+    nome: 'Peru',
+    pontos: 0
+};
+
+let Uruguai = {
+    ger: 82,
+    continente: 'AméricaSul',
+    bandeira: 'img/uruguai.png',
+    nome: 'Uruguai',
+    pontos: 0
+};
+
+let Paraguai = {
+    ger: 72,
+    continente: 'AméricaSul',
+    bandeira: 'img/paraguai.png',
+    nome: 'Paraguai',
+    pontos: 0
+};
+
+let Equador = {
+    ger: 75,
+    continente: 'AméricaSul',
+    bandeira: 'img/equador.png',
+    nome: 'Equador',
+    pontos: 0
+};
+
+let França = {
+    ger: 89,
+    continente: 'Europa',
+    bandeira: 'img/frança.png',
+    nome: 'França',
+    pontos: 0
+};
+
+let Portugal = {
+    ger: 80,
+    continente: 'Europa',
+    bandeira: 'img/portugal.png',
+    nome: 'Portugal',
+    pontos: 0
+};
+
+let Inglaterra = {
+    ger: 85,
+    continente: 'Europa',
+    bandeira: 'img/inglaterra.png',
+    nome: 'Inglaterra',
+    pontos: 0
+};
+
+let Holanda = {
+    ger: 84,
+    continente: 'Europa',
+    bandeira: 'img/holanda.png',
+    nome: 'Holanda',
+    pontos: 0
+};
+
+let Alemanha = {
+    ger: 83,
+    continente: 'Europa',
+    bandeira: 'img/alemanha.png',
+    nome: 'Alemanha',
+    pontos: 0
+};
+
+let Croacia = {
+    ger: 83,
+    continente: 'Europa',
+    bandeira: 'img/croacia.png',
+    nome: 'Croacia',
+    pontos: 0
+};
+
+let PaisDeGales = {
+    ger: 73,
+    continente: 'Europa',
+    bandeira: 'img/paisdegales.png',
+    nome: 'PaisDeGales',
+    pontos: 0
+};
+
+let Polonia = {
+    ger: 78,
+    continente: 'Europa',
+    bandeira: 'img/polonia.png',
+    nome: 'Polonia',
+    pontos: 0
+};
+
+let Dinamarca = {
+    ger: 73,
+    continente: 'Europa',
+    bandeira: 'img/dinamarca.png',
+    nome: 'Dinamarca',
+    pontos: 0
+};
+
+let Espanha = {
+    ger: 82,
+    continente: 'Europa',
+    bandeira: 'img/espanha.png',
+    nome: 'Espanha',
+    pontos: 0
+};
+
+let Belgica = {
+    ger: 77,
+    continente: 'Europa',
+    bandeira: 'img/belgica.png',
+    nome: 'Belgica',
+    pontos: 0
+};
+
+let Suiça = {
+    ger: 76,
+    continente: 'Europa',
+    bandeira: 'img/suiça.png',
+    nome: 'Suiça',
+    pontos: 0
+};
+
+let Servia = {
+    ger: 74,
+    continente: 'Europa',
+    bandeira: 'img/servia.png',
+    nome: 'Servia',
+    pontos: 0
+};
+
+let Camaroes = {
+    ger: 75,
+    continente: 'Africa',
+    bandeira: 'img/camaroes.png',
+    nome: 'Camaroes',
+    pontos: 0
+};
+
+let Gana = {
+    ger: 74,
+    continente: 'Africa',
+    bandeira: 'img/gana.png',
+    nome: 'Gana',
+    pontos: 0
+};
+
+let Tunisia = {
+    ger: 75,
+    continente: 'Africa',
+    bandeira: 'img/tunisia.png',
+    nome: 'Tunisia',
+    pontos: 0
+};
+
+let Senegal = {
+    ger: 77,
+    continente: 'Africa',
+    bandeira: 'img/senegal.png',
+    nome: 'Senegal',
+    pontos: 0
+};
+
+let Marrocos = {
+    ger: 79,
+    continente: 'Africa',
+    bandeira: 'img/marrocos.png',
+    nome: 'Marrocos',
+    pontos: 0
+};
+
+let Japão = {
+    ger: 76,
+    continente: 'Asia',
+    bandeira: 'img/japão.png',
+    nome: 'Japão',
+    pontos: 0
+};
+
+let CoreiaDoSul = {
+    ger: 75,
+    continente: 'Asia',
+    bandeira: 'img/CoreiaDoSul.png',
+    nome: 'CoreiaDoSul',
+    pontos: 0
+};
+
+let Australia = {
+    ger: 76,
+    continente: 'Asia',
+    bandeira: 'img/Australia.png',
+    nome: 'Australia',
+    pontos: 0
+};
+
+let Irã = {
+    ger: 74,
+    continente: 'Asia',
+    bandeira: 'img/Irã.png',
+    nome: 'Irã',
+    pontos: 0
+};
+
+let ArábiaSaudita = {
+    ger: 74,
+    continente: 'Asia',
+    bandeira: 'img/arábiasaudita.png',
+    nome: 'ArábiaSaudita',
+    pontos: 0
+};
+
+let Catar = {
+    ger: 74,
+    continente: 'Asia',
+    bandeira: 'img/catar.png',
+    nome: 'Catar',
+    pontos: 0
+};
+
+let EstadosUnidos = {
+    ger: 79,
+    continente: 'AméricaNorte',
+    bandeira: 'img/estadosunidos.png',
+    nome: 'EstadosUnidos',
+    pontos: 0
+};
+
+let México = {
+    ger: 75,
+    continente: 'AméricaNorte',
+    bandeira: 'img/méxico.png',
+    nome: 'México',
+    pontos: 0
+};
+
+let CostaRica = {
+    ger: 74,
+    continente: 'AméricaNorte',
+    bandeira: 'img/costarica.png',
+    nome: 'CostaRica',
+    pontos: 0
+};
+
+let Canadá = {
+    ger: 73,
+    continente: 'AméricaNorte',
+    bandeira: 'img/canadá.png',
+    nome: 'Canadá',
+    pontos: 0
+}
+
+let Itália = {
+    ger: 83,
+    continente: 'Europa',
+    bandeira: 'img/itália.png',
+    nome: 'Itália',
+    pontos: 0
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+let allcountrys = [Brasil, Argentina, Bolivia, Chile, Colombia, Equador, Paraguai, Peru, Uruguai, Venezuela, França, Portugal, Inglaterra, Holanda, Alemanha, Croacia, Itália, PaisDeGales, Polonia, Dinamarca, Espanha, Belgica, Suiça, Servia, Camaroes, Marrocos, Gana, Tunisia, Senegal, Japão, CoreiaDoSul, Australia, Irã, ArábiaSaudita, Catar, EstadosUnidos, México, CostaRica, Canadá];
+let confronto = [];
+let mainEl = document.querySelector('#lobby');
+let allEl = document.querySelector('#todos');
+let ConfrontoEl = document.querySelector('#confrontoAtual');
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+function criarPaises() {
+    let i = 1;
+    ConfrontoEl.innerHTML = "";
+    for (let pais of confronto) {
+        let flag = document.createElement("img");
+        ConfrontoEl.appendChild(flag);
+        flag.src = pais.bandeira;
+        flag.classList.add(pais.ger);
+        flag.classList.add(pais.nome);
+        flag.classList.add(pais.continente);
+        flag.draggable = false;
+        while (i > 0) {
+            let v = document.createElement("img");
+            ConfrontoEl.appendChild(v);
+            v.src = 'img/versus.png';
+            i--;
+        }
+    }
+}
+function escolherAleatorioOponentes() {
+    confronto = [];
+    let Random1 = getRandomInt(allcountrys.length);
+    let oponente1 = allcountrys[Random1];
+    let Random2 = getRandomInt(allcountrys.length);
+    while (Random2 == Random1) {
+        Random2 = getRandomInt(allcountrys.length);
+    }
+    let oponente2 = allcountrys[Random2];
+    confronto.push(oponente1);
+    confronto.push(oponente2);
+    criarPaises();
+}
+let selected = 1;
+function selecionar(e) {
+    if (e.currentTarget.classList.contains('oponente1') == false) {
+        e.currentTarget.classList.toggle('oponente' + selected);
+        selected++;
+    } else {
+        selected = 1;
+        e.currentTarget.classList.toggle('oponente' + selected);
+    }
+    if (selected == 3) {
+        botaoAleatorioEl.style.display = 'grid';
+        let oponente1 = document.querySelector('.oponente1');
+        let oponente2 = document.querySelector('.oponente2');
+        let i = 0;
+        for (let pais of allcountrys) {
+            if (pais.nome == oponente1.classList[1])
+                confronto.push(allcountrys[i]);
+            if (pais.nome == oponente2.classList[1])
+                confronto.push(allcountrys[i]);
+            i++;
+        }
+        criarPaises();
+        for (let bandeira of bandeiraEscolherEl) {
+            bandeira.removeEventListener('click', selecionar);
+            bandeira.classList.remove('oponente1');
+            bandeira.classList.remove('oponente2');
+        }
+    }
+}
+function escolherOponentes() {
+    confronto = [];
+    selected = 1;
+    ConfrontoEl.innerHTML = "";
+    botaoAleatorioEl.style.display = 'none';
+    bandeiraEscolherEl = document.querySelectorAll('img');
+    for (let bandeira of bandeiraEscolherEl) {
+        bandeira.addEventListener('click', selecionar)
+    }
+}
+
+
+let resultadoEl = document.querySelector('#resultado');
+function calcularResultado() {
+    let ganhador;
+    let maior;
+    let menor;
+    if (confronto[0].ger > confronto[1].ger) {
+        maior = confronto[0];
+        menor = confronto[1];
+    }
+    else {
+        maior = confronto[1];
+        menor = confronto[0];
+    }
+
+    let diferenca = maior.ger - menor.ger;
+    let chanceGol1;
+    let chanceGol2;
+    max = parseInt(diferenca / 4) + 1;
+    diferenca *= 2.5;
+    diferenca += 50;
+    if (confronto[0].ger < confronto[1].ger) {
+        chanceGol1 = 100 - diferenca;
+        chanceGol2 = diferenca;
+    } else {
+        chanceGol1 = diferenca;
+        chanceGol2 = 100 - diferenca;
+    }
+    let Ngols1 = 0;
+    let Ngols2 = 0;
+    Ngols1 += placar(chanceGol1, Ngols1, max);
+    Ngols2 += placar(chanceGol2, Ngols2, max);
+    let MudançaDeGer = 1; //ESCOLHA A ALTERAÇÃO DO GER
+    if (Ngols1 == Ngols2) {
+        resultadoEl.innerHTML = Ngols1 + ' X ' + Ngols2 + '<br>' + 'Empate' + '<br>' + 'Penalidades: ' + confronto[getRandomInt(2)].nome;
+    }
+    else if (Ngols1 > Ngols2) {
+        ganhador = confronto[0].nome;
+        confronto[0].ger += MudançaDeGer; //aumenta o ger//
+        if (confronto[0].ger > 100)
+            confronto[0].ger = 100;
+        confronto[1].ger -= MudançaDeGer; //diminui o ger//
+        if (confronto[1].ger < 55)
+            confronto[1].ger = 55
+        resultadoEl.innerHTML = Ngols1 + ' X ' + Ngols2 + '<br>' + ganhador + ' venceu!';
+    }
+    else {
+        ganhador = confronto[1].nome;
+        confronto[1].ger += MudançaDeGer; //aumenta o ger//
+        if (confronto[1].ger > 100)
+            confronto[1].ger = 100;
+        confronto[0].ger -= MudançaDeGer; //diminui o ger//
+        if (confronto[0].ger < 55)
+            confronto[0].ger = 55
+        resultadoEl.innerHTML = Ngols1 + ' X ' + Ngols2 + '<br>' + ganhador + ' venceu!';
+    }
+    allEl.innerHTML = "";
+    criar();
+    let flags = document.querySelectorAll('.colecao');
+    for (let pais of flags) {
+        adicionarInfo(pais);
+    }
+}
+function placar(chance, gols, quantidade) {
+    while (quantidade > 0) {
+        if (getRandomInt(100) <= chance) {
+            gols++;
+        }
+        quantidade--;
+    }
+    return gols;
+}
+
+function criar() {
+    for (let pais of allcountrys) {
+        let flag = document.createElement("img");
+        allEl.appendChild(flag);
+        flag.src = pais.bandeira;
+        flag.classList.add(pais.ger);
+        flag.classList.add(pais.nome);
+        flag.classList.add(pais.continente);
+        flag.classList.add('colecao');
+        flag.draggable = false;
+    }
+}
+criar();
+let botaoAleatorioEl = document.querySelector('#aleatorio');
+botaoAleatorioEl.addEventListener('click', escolherAleatorioOponentes);
+
+let botaoResultadoEl = document.querySelector('#partida');
+botaoResultadoEl.addEventListener('click', calcularResultado);
+
+let botaoEscolherEl = document.querySelector('#escolher');
+botaoEscolherEl.addEventListener('click', escolherOponentes);
+
+let flags = document.querySelectorAll('img');
+for (let pais of flags) {
+    adicionarInfo(pais);
+}
+
+let infoCardEl = document.querySelector('#ger');
+function mostrarInformacoes(e) {
+    infoCardEl.style.left = `calc(${e.pageX}px + 1vh)`;
+    infoCardEl.style.top = `calc(${e.pageY}px - 5vh)`;
+    infoCardEl.innerHTML = 'Ger Atual: ' + e.currentTarget.classList[0];
+}
+function adicionarInfo(pais) {
+    pais.addEventListener('mouseover', mostrarInformacoes);
+    pais.addEventListener('mousemove', mostrarInformacoes);
+    pais.addEventListener('mouseout', function () {
+        infoCardEl.style.top = '-1000px';
+    });
+}
+
+let workshop = 0;
+
+while (workshop > 0) {
+    botaoAleatorioEl.click();
+    botaoResultadoEl.click();
+    console.log('oi');
+    workshop--;
+}
